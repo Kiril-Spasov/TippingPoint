384a2bfbdb65afbf33aa831425598cbccc8170df
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-22 12:53:17 +0200

    Add project files.

---
+++ TippingPoint/FormTippingPoint.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TippingPoint
+{
+    partial class FormTippingPoint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnDetermineValue = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnDetermineValue
+            // 
+            this.BtnDetermineValue.Location = new System.Drawing.Point(51, 71);
+            this.BtnDetermineValue.Name = "BtnDetermineValue";
+            this.BtnDetermineValue.Size = new System.Drawing.Size(195, 70);
+            this.BtnDetermineValue.TabIndex = 0;
+            this.BtnDetermineValue.Text = "Determine Top Face Value";
+            this.BtnDetermineValue.UseVisualStyleBackColor = true;
+            this.BtnDetermineValue.Click += new System.EventHandler(this.BtnDetermineValue_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(306, 71);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(387, 319);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormTippingPoint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnDetermineValue);
+            this.Name = "FormTippingPoint";
+            this.Text = "Tipping Point";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnDetermineValue;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ TippingPoint/FormTippingPoint.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TippingPoint
+{
+    public partial class FormTippingPoint : Form
+    {
+        public FormTippingPoint()
+        {
+            InitializeComponent();
+        }
+
+        //Initial positions of the die.
+        int top = 1;
+        int front = 2;
+        int right = 3;
+        int left = 4;
+        int back = 5;
+        int bottom = 6;
+
+        private void BtnDetermineValue_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\tipping.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    Tip(line);
+                }
+            }
+            TxtResult.Text += "Final top value is: " + top;
+        }
+
+        private void Tip(string instruction)
+        {
+            switch (instruction)
+            {
+                //We know as per assignment that two opposite sides sum up to 7.
+                case "AWAY":
+                    top = front;
+                    front = bottom;
+                    bottom = 7 - top;
+                    back = 7 - front;
+                    break;
+                case "TOWARD":
+                    front = top;
+                    top = back;
+                    back = 7 - front;
+                    bottom = 7 - top;
+                    break;
+                case "LEFT":
+                    left = top;
+                    top = right;
+                    right = 7 - left;
+                    bottom = 7 - top;
+                    break;
+                case "RIGHT":
+                    right = top;
+                    top = left;
+                    left = 7 - right;
+                    bottom = 7 - top;
+                    break;
+            }
+        }
+    }
+}
